184c5ca6e237c6136def7d657ad0e00e3d39f5af
github username <github email address>
2021-06-07 22:57:36 -0400

    commit tree a4c5edb543a8e633b7fc59786fae35bcf52457dc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+ #using resrouces gives you both a patch and put instead of just patch like the below
+#  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article 
+#  patch 'articles/:id', to: 'articles#update' 
 end
